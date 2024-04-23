9a3489d772c4583973bd3f7340d606491108c5be
CokeLee777 <akak640@kyonggi.ac.kr>
2024-04-23 18:31:33 +0900

    [FEAT] 섹션 3 - max.in.flight.requests.per.connection 파라미터와 배치 메시지 전송 순서 이해까지 정리본 추가

--- src/chaemin.lee/KafkaProj-01/producers/src/main/java/com/example/kafka/PizzaProducer.java
+++ src/chaemin.lee/KafkaProj-01/producers/src/main/java/com/example/kafka/PizzaProducer.java
@@ -107,6 +107,7 @@ public class PizzaProducer {
     props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
     props.setProperty(
         ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+    props.setProperty(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, "50000");
 
     // acks setting
 //    props.setProperty(ProducerConfig.ACKS_CONFIG, "0");
