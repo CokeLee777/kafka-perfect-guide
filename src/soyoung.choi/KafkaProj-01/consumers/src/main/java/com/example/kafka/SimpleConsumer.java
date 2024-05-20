2c7f2e515b2588e71f5e7af82de3046dd0253014
SOYOUNG CHOI <66711073+cso6005@users.noreply.github.com>
2024-05-20 21:44:22 +0900

    add section4 consumer 실습 파일

---
+++ src/soyoung.choi/KafkaProj-01/consumers/src/main/java/com/example/kafka/SimpleConsumer.java
@@ -0,0 +1,46 @@
+package com.example.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
+
+public class SimpleConsumer {
+
+    public static final Logger logger = LoggerFactory.getLogger(SimpleConsumer.class.getName());
+
+    public static void main(String[] args) {
+
+        Properties props = new Properties();
+
+        String topicName = "simple-topic";
+
+        // 필수 consumer config
+        props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.56.101:9092");
+        props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group_01");
+
+        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<String, String>(props);
+
+        kafkaConsumer.subscribe(List.of(topicName));
+
+        while (true) {
+            ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(Duration.ofMillis(1000)); // 배치 단위 메시지 // 1초 동안 기다리게
+
+            for(ConsumerRecord  record: consumerRecords) {
+                logger.info("record key : {}, record value : {}, partition : {}"
+                        , record.key(), record.value(), record.partition());
+            }
+        }
+        // close 를 명시적으로 해줘야 함. 하면 meta 정보까지 정리됨.
+        // kafkaConsumer.close();
+    }
+}
