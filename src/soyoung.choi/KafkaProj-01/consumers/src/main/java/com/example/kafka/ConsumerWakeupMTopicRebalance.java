af94ebba1e0bc7959eb9076d27a4efee6038753b
SOYOUNG CHOI <66711073+cso6005@users.noreply.github.com>
2024-06-02 12:06:17 +0900

    update section 5 실습 코드
    - consumer 파티션 할당 전략

--- src/soyoung.choi/KafkaProj-01/consumers/src/main/java/com/example/kafka/ConsumerWakeupMTopicRebalance.java
+++ src/soyoung.choi/KafkaProj-01/consumers/src/main/java/com/example/kafka/ConsumerWakeupMTopicRebalance.java
@@ -1,9 +1,6 @@
 package com.example.kafka;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.common.errors.WakeupException;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
@@ -27,7 +24,11 @@ public class ConsumerWakeupMTopicRebalance {
         props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.56.101:9092");
         props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group-mtopic");
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group-assign");
+
+        // Consumer 파티션 할당 전략
+//        props.setProperty(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY_CONFIG, RoundRobinAssignor.class.getName());
+        props.setProperty(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY_CONFIG, CooperativeStickyAssignor.class.getName());
 
         KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<String, String>(props);
         kafkaConsumer.subscribe(List.of("topic-p3-t1", "topic-p3-t2"));
